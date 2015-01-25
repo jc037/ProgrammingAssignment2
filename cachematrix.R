5d3d284bc3c8c287a093238c9462da2e05da331d
jc037 <jacob037@yahoo.com>
2015-01-24 17:08:29 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions work together to cache a matrix based on user-input, 
+## and solve the inverse of that matrix if is new, or return the previous inverse if it has not changed
 
-## Write a short comment describing this function
+## This creates a matrix as defined by the user-input, defines a function to retrieve that matrix, and sets i to null to indicate to cacheSolve that the cache has been updated.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function checks if i above is not null (indicating it has been reset) and if it is not, returns the same value. 
+## If it detects i is null, it computes the inverse. This effectively prevents a computation if the matrix has not been changed.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i2 <- x$getinverse()
+  if(!is.null(i2)) {
+    message("getting cached data")
+    return(i2)
+  }
+  data <- x$get()
+  i2 <- solve(data, ...)
+  x$setinverse(i2)
+  i2
 }
+
+        ## Return a matrix that is the inverse of 'x'
